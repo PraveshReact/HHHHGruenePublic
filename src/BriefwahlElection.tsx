2949b3325d2351c398ba783e80e2a0271484b887
Kamal Kishore <kishorekamalsharma07@gmail.com>
2025-01-20 19:29:06 +0530

    new

--- src/BriefwahlElection.tsx
+++ src/BriefwahlElection.tsx
@@ -1,156 +1,150 @@
-import React, { useState } from 'react';
-import axios from 'axios';
-import '../src/CSS/ContactForm.css'; // Import the CSS file for styling
-import AlertPopup from './AlertPopup';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "../src/CSS/ContactForm.css"; // Import the CSS file for styling
+import '../src/CSS/ButtonStyle.css';
+import AlertPopup from "./AlertPopup";
 
 const BriefwahlElection = () => {
-    const [showAlert, setShowAlert] = useState(false);
-    const [alertMessage, setAlertMessage] = useState('');
-    const [formData, setFormData] = useState({
-        FirstName: '',
-        LastName: '',
-        Country: '',
-        Comment: '',
-        Created: new Date().toISOString().slice(0, 19).replace('T', ' '),
-        // acceptPrivacyPolicy: false,
-        // subscribeNewsletter: false
-    });
+  const [showAlert, setShowAlert] = useState(false);
+  const [alertMessage, setAlertMessage] = useState("");
+  const [isButtonDisabled, setIsButtonDisabled] = useState(true); // Initially disabled
+  const [formData, setFormData] = useState({
+    FirstName: "",
+    LastName: "",
+    Country: "",
+    Comment: "",
+    Created: new Date()
+      .toISOString()
+      .slice(0, 19)
+      .replace("T", " "),
+  });
 
-    // Handle form input changes
-    const handleChange = (e) => {
-        const { name, value, type, checked } = e.target;
-        setFormData(prevState => ({
-            ...prevState,
-            [name]: type === 'checkbox' ? checked : value
-        }));
-    };
+  // Handle form input changes
+  const handleChange = (e) => {
+    const { name, value, type, checked } = e.target;
+    setFormData((prevState) => ({
+      ...prevState,
+      [name]: type === "checkbox" ? checked : value,
+    }));
+  };
 
-    // Handle form submission
-    const handleSubmit = async (e) => {
-        e.preventDefault();
+  // Function to check if all fields are filled
+  const validateForm = () => {
+    const { FirstName, LastName, Country, Comment } = formData;
+    return FirstName && LastName && Country && Comment;
+  };
 
-        const apiEndpoint = 'https://gruene-weltweit.de/SPPublicAPIs/insertelectiondata.php';
+  // Update button disabled state when form data changes
+  useEffect(() => {
+    setIsButtonDisabled(!validateForm());
+  }, [formData]); // Run this check every time formData changes
 
-        try {
-            // Send POST request with form data
-            const response = await axios.post(apiEndpoint, formData);
+  // Handle form submission
+  const handleSubmit = async (e) => {
+    e.preventDefault();
 
-            // Handle success
-            console.log('Form submitted successfully:', response.data);
-            setAlertMessage('Vielen Dank für Deine Hilfe!');
-            setShowAlert(true);
-            setFormData({
-                FirstName: '',
-                LastName: '',
-                Country: '',
-                Comment: '',
-                Created: new Date().toISOString().slice(0, 19).replace('T', ' '),
-                // acceptPrivacyPolicy: false,
-                // subscribeNewsletter: false
-            });
+    const apiEndpoint =
+      "https://gruene-weltweit.de/SPPublicAPIs/insertelectiondata.php";
 
-        } catch (error) {
-            // Handle error
-            console.error('Error submitting form:', error);
-            alert('There was an error submitting your form. Please try again later.');
-        }
-    };
-    const handleCloseAlert = () => {
-        setShowAlert(false);
-      };
+    try {
+      // Send POST request with form data
+      const response = await axios.post(apiEndpoint, formData);
 
-    // Check if the privacy policy checkbox is checked
-   // const isFormValid = formData.acceptPrivacyPolicy; // The form will only be valid if this is true
+      // Handle success
+      console.log("Form submitted successfully:", response.data);
+      setAlertMessage("Vielen Dank für Deine Hilfe!");
+      setShowAlert(true);
+      setFormData({
+        FirstName: "",
+        LastName: "",
+        Country: "",
+        Comment: "",
+        Created: new Date()
+          .toISOString()
+          .slice(0, 19)
+          .replace("T", " "),
+      });
+      setIsButtonDisabled(true); // Disable the button after submit
+    } catch (error) {
+      // Handle error
+      console.error("Error submitting form:", error);
+      alert("There was an error submitting your form. Please try again later.");
+    }
+  };
 
-    return (
-        <div className="contact-form-bg">
-            <div className="contact-form-container">
-                <h2 className="contact-form-title">Wahlkampf-aus-der-Ferne</h2>
-                <form className="contact-form" onSubmit={handleSubmit}>
-                    <div className="input-group">
-                        <label htmlFor="FirstName">First Name</label>
-                        <input
-                            type="text"
-                            id="FirstName"
-                            name="FirstName"
-                            value={formData.FirstName}
-                            onChange={handleChange}
-                            className="form-input m-0"
-                        />
-                    </div>
+  const handleCloseAlert = () => {
+    setShowAlert(false);
+  };
 
-                    <div className="input-group">
-                        <label htmlFor="LastName">Last Name</label>
-                        <input
-                            type="text"
-                            id="LastName"
-                            name="LastName"
-                            value={formData.LastName}
-                            onChange={handleChange}
-                            className="form-input m-0"
-                        />
-                    </div>
-                    <div className="input-group">
-                        <label htmlFor="Country">Country</label>
-                        <input
-                            type="text"
-                            id="Country"
-                            name="Country"
-                            value={formData.Country}
-                            onChange={handleChange}
-                            className="form-input m-0"
-                        />
-                    </div>
-                    <div className="input-group">
-                        <label htmlFor="Comment">Comment</label>
-                        <textarea
-                            id="Comment"
-                            name="Comment"
-                            value={formData.Comment}
-                            onChange={handleChange}
-                            required
-                            className="form-input m-0"
-                            rows={4} // Increase the rows to make the textarea taller
-                        />
-                    </div>
+  return (
+    <div className="contact-form-bg">
+      <div className="contact-form-container">
+        <h2 className="contact-form-title">Wahlkampf-aus-der-Ferne</h2>
+        <form className="contact-form">
+          <div className="input-group">
+            <label htmlFor="FirstName">First Name</label>
+            <input
+              type="text"
+              id="FirstName"
+              name="FirstName"
+              value={formData.FirstName}
+              onChange={handleChange}
+              className="form-input m-0"
+            />
+          </div>
 
-                    {/* <div className="checkbox-group">
-                        <label className="checkbox-label">
-                            <input className='form-check-input me-2'
-                                type="checkbox"
-                                name="acceptPrivacyPolicy"
-                                checked={formData.acceptPrivacyPolicy}
-                                onChange={handleChange}
-                                required
-                            />
-                            Ich akzeptiere die <a href="/Datenschutz" className="privacy-policy-link">Datenschutzerklärung</a>.
-                        </label>
-                    </div>
+          <div className="input-group">
+            <label htmlFor="LastName">Last Name</label>
+            <input
+              type="text"
+              id="LastName"
+              name="LastName"
+              value={formData.LastName}
+              onChange={handleChange}
+              className="form-input m-0"
+            />
+          </div>
 
-                    <div className="checkbox-group">
-                        <label className="checkbox-label">
-                            <input className='form-check-input me-2'
-                                type="checkbox"
-                                name="subscribeNewsletter"
-                                checked={formData.subscribeNewsletter}
-                                onChange={handleChange}
-                            />
-                            Ich möchte den Grüne-Weltweit-Newsletter beziehen.
-                        </label>
-                    </div> */}
+          <div className="input-group">
+            <label htmlFor="Country">Country</label>
+            <input
+              type="text"
+              id="Country"
+              name="Country"
+              value={formData.Country}
+              onChange={handleChange}
+              className="form-input m-0"
+            />
+          </div>
 
-                    <button
-                        type="submit"
-                        className="submit-button"
-                        //disabled={!isFormValid} // Disable the button if the privacy policy checkbox is not checked
-                    >
-                        Absenden
-                    </button>
-                </form>
-            </div>
-            {showAlert && <AlertPopup message={alertMessage} onClose={handleCloseAlert} />}
-        </div>
-    );
+          <div className="input-group">
+            <label htmlFor="Comment">Comment</label>
+            <textarea
+              id="Comment"
+              name="Comment"
+              value={formData.Comment}
+              onChange={handleChange}
+              required
+              className="form-input m-0"
+              rows={4}
+            />
+          </div>
+
+          <button
+            type="submit"
+            className={`submit-button ${isButtonDisabled ? "disabled" : ""}`}
+            disabled={isButtonDisabled}
+            onClick={handleSubmit}
+          >
+            Absenden
+          </button>
+        </form>
+      </div>
+      {showAlert && (
+        <AlertPopup message={alertMessage} onClose={handleCloseAlert} />
+      )}
+    </div>
+  );
 };
 
 export default BriefwahlElection;
